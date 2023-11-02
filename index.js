2c28f2f3daa856a796f142093c6dd4b0d1018ccb
Melvin Carvalho <melvincarvalho@gmail.com>
2023-11-02 02:56:17 +0100

    first release

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3100;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
