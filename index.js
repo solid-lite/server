3a999885518f2a7d565dd2ade1937d7fa5dcc132
Melvin Carvalho <melvincarvalho@gmail.com>
2023-11-06 08:11:32 +0100

    add bearer auth

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
 #!/usr/bin/env node
 
-import express from 'express';
-import fs from 'fs-extra';
-import path from 'path';
-import https from 'https';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from 'express'
+import fs from 'fs-extra'
+import path, { dirname } from 'path'
+import https from 'https'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const app = express();
-app.disable('x-powered-by');
+const app = express()
+app.disable('x-powered-by')
 app.use((req, res, next) => {
-  setCorsHeaders(res); // Apply CORS headers to every response
+  setCorsHeaders(res) // Apply CORS headers to every response
 
   if (req.method === 'OPTIONS') {
-    handleOptions(req, res); // Handle preflight OPTIONS requests
+    handleOptions(req, res) // Handle preflight OPTIONS requests
   } else {
-    next(); // Continue to other routes for non-OPTIONS requests
+    next() // Continue to other routes for non-OPTIONS requests
   }
-});
+})
 
-
-const dataDirectory = path.join(__dirname, 'data');
-const defaultFile = path.join(__dirname, 'profile.html');
+const dataDirectory = path.join(__dirname, 'data')
+const defaultFile = path.join(__dirname, 'profile.html')
 
 // Enable JSON and URL Encoded parsers
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Serve static files from the data directory at the root URL
-app.use(express.static(dataDirectory));
+//
+/// / Define the options for express.static
+const options = {
+  setHeaders: function (res, filePath, stat) { // Change 'path' to 'filePath' to avoid conflict with 'path' module
+    // Serve files without an extension as 'text/html'
+    if (!path.extname(filePath)) { // Use 'path.extname' method correctly on 'filePath'
+      res.set('Content-Type', 'text/html')
+    }
+  }
+}
+
+// Serve static files from the 'public' directory
+app.use(express.static(dataDirectory, options))
+
+// Add middleware to protect routes
+const protectRoutes = (req, res, next) => {
+  if (['PUT', 'POST', 'DELETE'].includes(req.method)) {
+    const authHeader = req.headers.authorization
+    const apiKey = process.env.SOLID_API_KEY
+
+    // Check if the Authorization header is present and formatted correctly
+    if (!authHeader || !authHeader.startsWith('Bearer ') || authHeader.split(' ')[1] !== apiKey) {
+      return res.status(401).send('Unauthorized')
+    }
+  }
+  next()
+}
+
+app.use(protectRoutes)
+
+// app.use(express.static(dataDirectory));
 
 // Ensure data directory exists
-fs.ensureDirSync(dataDirectory);
-const defaultFilePath = path.join(dataDirectory, 'index.html'); // Path for index.html in the data directory
+fs.ensureDirSync(dataDirectory)
+const defaultFilePath = path.join(dataDirectory, 'index.html') // Path for index.html in the data directory
 if (!fs.existsSync(defaultFilePath)) {
   // Copy profile.html to index.html if index.html does not exist
-  fs.copySync(path.join(__dirname, 'profile.html'), defaultFilePath);
+  fs.copySync(path.join(__dirname, 'profile.html'), defaultFilePath)
 }
 
 /**
@@ -121,125 +148,127 @@ const getContentType = (ext) => {
   }
 }
 
-
 function setCorsHeaders(res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('X-Powered-By', 'solid-lite-0.0.1');
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.setHeader('X-Powered-By', 'solid-lite-0.0.1')
 }
 
 function handleOptions(req, res) {
-  setCorsHeaders(res);
-  res.status(204).end(); // No Content response for preflight requests
+  setCorsHeaders(res)
+  res.status(204).end() // No Content response for preflight requests
 }
 
-
-
-
-
 app.options('*', (req, res) => {
-  setCorsHeaders(res);
-  res.status(204).end();
-});
+  setCorsHeaders(res)
+  res.status(204).end()
+})
 
-
-app.use(express.text()); // Add this line to handle text/plain content type
+app.use(express.text()) // Add this line to handle text/plain content type
 app.use((req, res, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} Request to ${req.originalUrl}`);
-  console.log('Body:', req.body);
-  next();
-});
+  console.log(`${new Date().toISOString()} - ${req.method} Request to ${req.originalUrl}`)
+  console.log('Body:', req.body)
+  next()
+})
 
 // CRUD operations
 
 // CREATE: Upload a new file
 app.post('/data/:filename', (req, res) => {
-  const { filename } = req.params;
-  const filePath = path.join(dataDirectory, filename);
-  fs.outputFileSync(filePath, req.body.content);
-  res.status(201).send('File created successfully.');
-});
+  const { filename } = req.params
+  const filePath = path.join(dataDirectory, filename)
+  fs.outputFileSync(filePath, req.body.content)
+  res.status(201).send('File created successfully.')
+})
 
 // HEAD: Retrieve the headers of a file
 // HEAD: Retrieve the headers of a file
 app.head('/data/:filename', (req, res) => {
-  const { filename } = req.params;
-  const filePath = path.join(dataDirectory, filename);
+  const { filename } = req.params
+  const filePath = path.join(dataDirectory, filename)
 
   if (fs.existsSync(filePath)) {
     fs.stat(filePath, (err, stats) => {
       if (err) {
-        console.error(err);
-        return res.status(500).send('An error occurred while retrieving the file info.');
+        console.error(err)
+        return res.status(500).send('An error occurred while retrieving the file info.')
       }
 
       // Get the file extension from the filename
-      const ext = path.extname(filename);
+      const ext = path.extname(filename)
       // Determine the content type
-      const contentType = getContentType(ext);
+      const contentType = getContentType(ext)
 
-      res.setHeader('Content-Type', contentType);
-      res.setHeader('Content-Length', stats.size);
-      res.setHeader('Last-Modified', stats.mtime.toUTCString());
-      res.status(200).end();
-    });
+      res.setHeader('Content-Type', contentType)
+      res.setHeader('Content-Length', stats.size)
+      res.setHeader('Last-Modified', stats.mtime.toUTCString())
+      res.status(200).end()
+    })
   } else {
-    res.status(404).send('File not found.');
+    res.status(404).send('File not found.')
   }
-});
+})
 
 // READ: Get a list of files or a specific file
-app.get('/data/:filename?', (req, res) => {
-  const { filename } = req.params;
+app.get('/:filename?', (req, res) => {
+  const { filename } = req.params
+  console.log('filename', filename)
   if (filename) {
-    const filePath = path.join(dataDirectory, filename);
+    const filePath = path.join(dataDirectory, filename)
     if (fs.existsSync(filePath)) {
-      res.sendFile(filePath);
+      // Get the file extension from the filename
+      const ext = path.extname(filename)
+      // Determine the content type
+      const contentType = getContentType(ext)
+      console.log('Content-Type', contentType)
+
+      res.setHeader('Content-Type', contentType)
+
+      res.sendFile(filePath)
     } else {
-      res.status(404).send('File not found.');
+      res.status(404).send('File not found.')
     }
   } else {
-    const files = fs.readdirSync(dataDirectory);
-    res.json(files);
+    const files = fs.readdirSync(dataDirectory)
+    res.json(files)
   }
-});
+})
 
 // CREATE or UPDATE: Upload or update a file
 app.put('/:filename', (req, res) => {
-  const { filename } = req.params;
-  const filePath = path.join(dataDirectory, filename);
+  const { filename } = req.params
+  const filePath = path.join(dataDirectory, filename)
 
   fs.outputFile(filePath, req.body, err => {
     if (err) {
-      console.error(err);
-      res.status(500).send('An error occurred while writing the file.');
+      console.error(err)
+      res.status(500).send('An error occurred while writing the file.')
     } else {
-      res.send('File created/updated successfully.');
+      res.send('File created/updated successfully.')
     }
-  });
-});
+  })
+})
 
 // DELETE: Delete a file
 app.delete('/:filename', (req, res) => {
-  const { filename } = req.params;
-  const filePath = path.join(dataDirectory, filename);
+  const { filename } = req.params
+  const filePath = path.join(dataDirectory, filename)
   if (fs.existsSync(filePath)) {
-    fs.removeSync(filePath);
-    res.send('File deleted successfully.');
+    fs.removeSync(filePath)
+    res.send('File deleted successfully.')
   } else {
-    res.status(404).send('File not found.');
+    res.status(404).send('File not found.')
   }
-});
-
+})
 
 // Read SSL certificate and private key
-const privateKey = fs.readFileSync(path.join(__dirname, 'certs', 'privkey.pem'), 'utf8');
-const certificate = fs.readFileSync(path.join(__dirname, 'certs', 'fullchain.pem'), 'utf8');
+const privateKey = fs.readFileSync(path.join(__dirname, 'certs', 'privkey.pem'), 'utf8')
+const certificate = fs.readFileSync(path.join(__dirname, 'certs', 'fullchain.pem'), 'utf8')
 
-const credentials = { key: privateKey, cert: certificate };
+const credentials = { key: privateKey, cert: certificate }
 
-const PORT = process.env.PORT || 3111;
+const PORT = process.env.PORT || 3111
 https.createServer(credentials, app).listen(PORT, () => {
-  console.log(`Solid-Lite is running on https://localhost:${PORT}`);
-});
+  console.log(`Solid-Lite v0.0 is running on https://localhost:${PORT}`)
+})
